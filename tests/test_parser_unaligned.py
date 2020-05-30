1c614ecc68a642a3096a10c12fed44deb7df4950
Johan Nestaas <johannestaas@gmail.com>
2020-05-30 15:53:14 -0700

    add a simple unaligned unsigned parser to test since I skipped signed for now

--- tests/test_parser_unaligned.py
+++ tests/test_parser_unaligned.py
@@ -29,7 +29,7 @@ class LEData:
 
 
 @pytest.mark.skip
-def test_ledata():
+def test_ledata_ff():
     data, rest = LEData.parse(b'\xff' * 8)
     assert data.u8 == 0xff
     assert data.u4 == 0b1111
@@ -41,8 +41,44 @@ def test_ledata():
     assert data.i2_2 == -0b01
     assert data.i8_1 == -0b01111111
     assert data.i8_2 == -0b01111111
-    # assert data.i16 == -0x7fff
+    assert data.i16 == -0x7fff
     assert data.i4 == -0b0111
     assert data.i3 == -0b011
     assert data.u1_2 == 0b1
     assert rest == b''
+
+
+@parser
+class LESimple:
+    u8_1: le_u(8)
+    # 1 byte
+    u4_1: le_u(4)
+    u4_2: le_u(4)
+    # 2 byte
+    u3_1: le_u(3)
+    u1_1: le_u(1)
+    u3_2: le_u(3)
+    u1_2: le_u(1)
+    # 3 byte
+    u5_1: le_u(5)
+    u3_3: le_u(3)
+    # 4 byte
+    u16_1: le_u(16)
+    # 6 byte
+
+
+def test_lesimple_ff():
+    data, rest = LESimple.parse(b'\xff' * 8)
+    assert data == LESimple(
+        u8_1=0xff,
+        u4_1=0x0f,
+        u4_2=0x0f,
+        u3_1=0x07,
+        u1_1=0x01,
+        u3_2=0x07,
+        u1_2=0x01,
+        u5_1=0x1f,
+        u3_3=0x07,
+        u16_1=0xffff,
+    )
+    assert rest == b'\xff\xff'
